ac8f42ba258e0845117c40e6c427cefbc236a4ea
Faizan ul haq <mfaizanulhaq1>
2018-08-02 21:45:40 +0500

    first databse

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(koaBody({ multipart: true }));
 app.use(bodyParser());
 app.use(router.routes());
 
-db.sequelize.sync({force:true}).
+db.sequelize.sync({force:false}).
 then(()=>console.log("table generated")).
 catch((err)=>console.log(err))
 
