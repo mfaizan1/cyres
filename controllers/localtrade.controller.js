8e6d3d84dd56ccfbdd4bb537b2713c7700620289
Faizan ul haq <mfaizanulhaq1>
2018-08-02 22:11:04 +0500

    id fix

--- controllers/localtrade.controller.js
+++ controllers/localtrade.controller.js
@@ -181,7 +181,7 @@ async profile(ctx){
         let details=null;
         let totalTrades= null;
         console.log("here");
-    await ctx.db.sequelize.query('select "traders"."id" as "traderId","traders"."name" as "traderName","coinsToTrades"."id","coinsToTrades"."minQuantity","coinsToTrades"."maxQuantity","coinsToTrades"."pricePerTokken" , "verificationApplications"."status", \
+    await ctx.db.sequelize.query('select "traders"."id" as "traderId","traders"."name" as "traderName","coinsToTrades"."id","coinsToTrades"."minQuantity","coinsToTrades"."maxQuantity","coinsToTrades"."pricePerTokken","coinsToTrades"."supportedTokenId" , "verificationApplications"."status", \
     "supportedTokens"."name","supportedTokens"."symbol" \
     from "coinsToTrades" \
     full outer join "verificationApplications" on "verificationApplications"."traderId" = "coinsToTrades"."traderId" \
